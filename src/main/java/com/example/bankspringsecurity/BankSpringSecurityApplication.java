27c2b776f731f3666bdd9bb6fbe443a3f8aecfeb
yongckim <zxcv9203@naver.com>
2023-08-16 23:03:18 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/java/com/example/bankspringsecurity/BankSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSpringSecurityApplication.class, args);
+    }
+
+}
