bb804d3a47b4de610dfe39ac6bb304005575f44a
yongckim <zxcv9203@naver.com>
2023-09-05 22:24:47 +0900

    :sparkles: account 엔티티 구현

--- src/main/java/com/example/bankspringsecurity/domain/account/Account.java
+++ src/main/java/com/example/bankspringsecurity/domain/account/Account.java
@@ -1,5 +1,6 @@
-package com.example.bankspringsecurity.domain.user;
+package com.example.bankspringsecurity.domain.account;
 
+import com.example.bankspringsecurity.domain.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,30 +13,26 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
+@Table(name = "account")
 @EntityListeners(AuditingEntityListener.class)
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
-
+@Getter
+public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false, length = 20)
-    private String username;
-    @Column(nullable = false, length = 60)
-    private String password;
+    private Long number;    // 계좌 번호
 
-    @Column(nullable = false, length = 20)
-    private String email;
+    @Column(nullable = false, length = 4)
+    private Long password;  // 계좌 비밀번호
 
-    @Column(nullable = false, length = 20)
-    private String fullName;
-
-    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private UserRole role;
+    private Long balance;   // 잔액 (기본값 1000원)
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 
     @CreatedDate
     @Column(nullable = false)
@@ -46,13 +43,12 @@ public class User {
     private LocalDateTime updatedAt;
 
     @Builder
-    public User(Long id, String username, String password, String email, String fullName, UserRole role, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public Account(Long id, Long number, Long password, Long balance, User user, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
-        this.username = username;
+        this.number = number;
         this.password = password;
-        this.email = email;
-        this.fullName = fullName;
-        this.role = role;
+        this.balance = balance;
+        this.user = user;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
