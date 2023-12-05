915e55bcff0fdceb00b71d7c3f391a6727ddcc29
yongckim <zxcv9203@naver.com>
2023-12-06 00:11:47 +0900

    :white_check_mark: 로그인 실패시 401 반환 테스트 코드 작성

--- src/test/java/com/example/bankspringsecurity/config/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/com/example/bankspringsecurity/config/jwt/JwtAuthenticationFilterTest.java
@@ -7,6 +7,7 @@ import com.example.bankspringsecurity.stub.UserStub;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,16 +16,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Transactional
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class JwtAuthenticationFilterTest {
@@ -72,11 +74,28 @@ class JwtAuthenticationFilterTest {
     }
 
     @Test
-    void unSuccessfulAuthenticationTest() {
+    @DisplayName("[실패] 비밀번호를 잘못 입력시 로그인이 실패되어야 합니다.")
+    void unSuccessfulAuthenticationTest() throws Exception{
         // given
+        LoginRequest request = LoginRequest.builder()
+                .username("test")
+                .password("12345")
+                .build();
+        String requestBody = mapper.writeValueAsString(request);
+
 
         // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(requestBody)
+        );
+        String jwtToken = resultActions.andReturn()
+                .getResponse()
+                .getHeader(JwtVO.HEADER);
 
         // then
+        resultActions.andExpect(status().isUnauthorized());
     }
 }
