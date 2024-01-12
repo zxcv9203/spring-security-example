5eb79f5677788d7822eca4c5f4ef2ead0142e039
yongckim <zxcv9203@naver.com>
2024-01-12 18:30:08 +0900

    :wrench: 테스트를 위한 DB 정리 SQL문 테스트에 적용

--- src/test/java/com/example/bankspringsecurity/config/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/com/example/bankspringsecurity/config/jwt/JwtAuthenticationFilterTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@Transactional
+@Sql("classpath:db/teardown.sql")
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class JwtAuthenticationFilterTest {
--- src/test/java/com/example/bankspringsecurity/web/AccountControllerTest.java
+++ src/test/java/com/example/bankspringsecurity/web/AccountControllerTest.java
@@ -1,11 +1,9 @@
 package com.example.bankspringsecurity.web;
 
-import com.example.bankspringsecurity.domain.account.Account;
 import com.example.bankspringsecurity.domain.account.AccountRepository;
 import com.example.bankspringsecurity.domain.user.User;
 import com.example.bankspringsecurity.domain.user.UserRepository;
 import com.example.bankspringsecurity.dto.AccountSaveRequest;
-import com.example.bankspringsecurity.exception.CustomApiException;
 import com.example.bankspringsecurity.stub.AccountStub;
 import com.example.bankspringsecurity.stub.UserStub;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,9 +14,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.TestExecutionEvent;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,14 +24,12 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@Transactional
+@Sql("classpath:db/teardown.sql")
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class AccountControllerTest {
